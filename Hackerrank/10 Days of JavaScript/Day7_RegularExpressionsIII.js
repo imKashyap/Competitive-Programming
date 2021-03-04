51f1d74110d02b4955d84525550c8d85d0365b51
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-03-04 15:34:53 +0530

    Day 7

---
+++ Hackerrank/10 Days of JavaScript/Day7_RegularExpressionsIII.js
@@ -0,0 +1,13 @@
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    
+    var re = RegExp('\\d+', 'g');
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
